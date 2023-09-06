4210f2045e714a1281612b1a32830a6b74f70e85
shejask <59792228+shejask@users.noreply.github.com>
2023-09-07 02:20:29 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://adminUser:suVVOf6Vv6UNZqns@cluster0.6kubs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connect('mongodb://shejask9:shijask9@ac-jd41jmc-shard-00-00.xtmzc2m.mongodb.net:27017,ac-jd41jmc-shard-00-01.xtmzc2m.mongodb.net:27017,ac-jd41jmc-shard-00-02.xtmzc2m.mongodb.net:27017/?ssl=true&replicaSet=atlas-axbnd9-shard-0&authSource=admin&retryWrites=true&w=majority');
